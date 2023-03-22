1ed66e7c9f81ea51d26527b68b74b03cd39dfc14
Harsh kumar choudhary <harshkumarchoudhary144@gmail.com>
2023-03-22 13:30:16 +0530

    Added skeleton logic using Materials UI

--- src/components/DisplayCampaigns.jsx
+++ src/components/DisplayCampaigns.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { Skeleton } from '@mui/material';
 import FundCard from './FundCard';
 import { loader } from '../assets';
 
@@ -17,12 +17,17 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
 
       <div className="flex flex-wrap mt-[20px] gap-[26px]">
         {isLoading && (
-          <img src={loader} alt="loader" className="w-[100px] h-[100px] object-contain" />
+          <div className='grid grid-rows-1 grid-flow-col gap-4'>
+            <Skeleton animation="wave" variant="rectangular" width={288} height={385} />
+            <Skeleton animation="wave" variant="rectangular" width={288} height={385} />
+            <Skeleton animation="wave" variant="rectangular" width={288} height={385} />
+            <Skeleton animation="wave" variant="rectangular" width={288} height={385} />
+          </div>
         )}
 
         {!isLoading && campaigns.length === 0 && (
-          <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
-            You have not created any campigns yet
+          <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#ffffff]">
+            No Campaigns Yet.
           </p>
         )}
 
